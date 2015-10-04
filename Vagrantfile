0305eafe3dfe2d4c62d982a96e210dd9e49cefed
Ary Wibowo <nucreativa@gmail.com>
2015-10-04 10:05:43 +0700

    First Commit

---
+++ Vagrantfile
@@ -0,0 +1,16 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  config.vm.box = "trusty64"
+  config.vm.network "private_network", ip: "192.168.64.64"
+  config.vm.synced_folder "./public", "/var/www", create: true, group: "www-data", owner: "www-data"
+
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui    = true
+    vb.name   = "Vagrant"
+    vb.memory = "1024"
+  end
+
+  config.vm.provision "shell", path: "./provision/setup.sh"
+end
---
+++ provision/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Provisioning virtual machine..."
+
+echo "Updating Repository"
+    apt-get install python-software-properties build-essential -y > /dev/null
+    add-apt-repository ppa:ondrej/php5 -y > /dev/null
+    add-apt-repository ppa:chris-lea/node.js > /dev/null
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > /dev/null
+    echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list > /dev/null
+
+echo "Updating Ubuntu Packages"
+    apt-get update > /dev/null
+    apt-get upgrade > /dev/null
+
+echo "Installing Git"
+    apt-get install git -y > /dev/null
+
+echo "Installing Nginx"
+    apt-get install nginx -y > /dev/null
+
+echo "Installing PHP"
+    apt-get install php5-common php5-dev php5-cli php5-fpm curl php5-intl php5-mongo php5-memcache php5-mcrypt php5-mysql php5-gd php5-imagick php5-pgsql php5-sqlite php5-curl -y > /dev/null
+
+echo "Installing Composer"
+    curl -sS https://getcomposer.org/installer | php > /dev/null
+    mv composer.phar /usr/local/bin/composer > /dev/null
+    composer global require "fxp/composer-asset-plugin:~1.0.3" > /dev/null
+
+echo "Installing MongoDB"
+    apt-get install -y mongodb-org > /dev/null
+
+echo "Installing NodeJS"
+    apt-get install nodejs > /dev/null
+    apt-get install npm > /dev/null

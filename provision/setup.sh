dd119dca2652d26d3096af0c5904637c5302565a
Ary Wibowo <nucreativa@gmail.com>
2015-11-20 17:55:35 +0700

    Add repo ppa:ondrej/php5-5.6 to make sure PHP 5.6 installed

--- provision/setup.sh
+++ provision/setup.sh
@@ -8,7 +8,7 @@ echo "Updating Ubuntu Packages"
 
 echo "Updating Repository"
     apt-get install python-software-properties build-essential -y > /dev/null
-    add-apt-repository ppa:ondrej/php5 -y > /dev/null
+    add-apt-repository ppa:ondrej/php5-5.6 -y > /dev/null
     add-apt-repository ppa:chris-lea/node.js -y > /dev/null
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 > /dev/null
     echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list > /dev/null

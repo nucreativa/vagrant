df3928759ac0813b2306f09f15ff349c006ded1b
Ary Wibowo <nucreativa@gmail.com>
2016-02-14 20:35:03 +0700

    Update provision file, add RabbitMQ and refactoring the others

--- provision/setup.sh
+++ provision/setup.sh
@@ -18,13 +18,17 @@ echo "Installing Git"
 
 echo "Installing Nginx"
     add-apt-repository ppa:nginx/stable
-    apt-get update > /dev/null 2>&1
-    apt-get -y install nginx > /dev/null 2>&1
+    apt-get update && apt-get -y install nginx > /dev/null 2>&1
 
-echo "Installing PHP"
+#echo "Installing PHP 5.6"
+#    LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php5-5.6 -y > /dev/null 2>&1
+#    apt-get update > /dev/null 2>&1
+#    apt-get -y install php5-common php5-dev php5-fpm php5-cli php5-curl php5-intl php5-mysql php5-sqlite3 php5-mcrypt php5-gd > /dev/null 2>&1
+
+echo "Installing PHP 7"
     LC_ALL=en_US.UTF-8 add-apt-repository ppa:ondrej/php-7.0 -y > /dev/null 2>&1
     apt-get update > /dev/null 2>&1
-    apt-get -y install php7.0 php7.0-dev php7.0-fpm php7.0-cli php7.0-curl php7.0-intl php7.0-mysql php7.0-sqlite3 > /dev/null 2>&1
+    apt-get -y install php7.0 php7.0-common php7.0-dev php7.0-fpm php7.0-cli php7.0-curl php7.0-intl php7.0-mysql php7.0-sqlite3 php7.0-mcrypt php7.0-gd > /dev/null 2>&1
 
 echo "Installing Composer"
     curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1
@@ -37,10 +41,9 @@ echo "Installing SQLite3"
 echo "Installing MariaDB"
     apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db > /dev/null 2>&1
     add-apt-repository 'deb [arch=amd64,i386] http://mariadb.biz.net.id/repo/10.1/ubuntu trusty main'  > /dev/null 2>&1
-    apt-get update > /dev/null 2>&1
     echo "mysql-server mysql-server/root_password password $DBPASSWD" | debconf-set-selections
     echo "mysql-server mysql-server/root_password_again password $DBPASSWD" | debconf-set-selections
-    apt-get -y install mariadb-server > /dev/null 2>&1
+    apt-get update && apt-get -y install mariadb-server > /dev/null 2>&1
     mysql -uroot -p$DBPASSWD -e "grant all privileges on *.* to '$DBUSER'@'%' identified by '$DBPASSWD'"
 
 echo "Installing Redis"
@@ -57,8 +60,17 @@ echo "Installing Memcached"
 echo "Installing MongoDB"
     apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 > /dev/null 2>&1
     echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list > /dev/null 2>&1
-    apt-get update > /dev/null 2>&1
-    apt-get -y install pkg-config libsasl2-dev mongodb-org > /dev/null 2>&1
+    apt-get update && apt-get -y install pkg-config libsasl2-dev mongodb-org > /dev/null 2>&1
     pecl install mongodb > /dev/null 2>&1
     echo "extension=/usr/lib/php/20151012/mongodb.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"` > /dev/null 2>&1
     service php7.0-fpm restart > /dev/null 2>&1
+
+echo "Installing RabbitMQ"
+    echo "deb http://www.rabbitmq.com/debian testing main" >> /etc/apt/sources.list > /dev/null 2>&1
+    wget https://www.rabbitmq.com/rabbitmq-signing-key-public.asc > /dev/null 2>&1
+    apt-key add rabbitmq-signing-key-public.asc > /dev/null 2>&1
+    apt-get update && apt-get -y install rabbitmq-server > /dev/null 2>&1
+    rabbitmq-plugins enable rabbitmq_management > /dev/null 2>&1
+    rabbitmqctl add_user vagrant vagrant > /dev/null 2>&1
+    rabbitmqctl set_user_tags vagrant administrator > /dev/null 2>&1
+    rabbitmqctl set_permissions -p / vagrant ".*" ".*" ".*" > /dev/null 2>&1
